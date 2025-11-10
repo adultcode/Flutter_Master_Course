c2546a7ccc68f42de838fa5e0cb878d2d8181ef0
hharddy <hharddy@yahoo.com>
2025-11-10 10:53:42 +0330

    S1E1

---
+++ S1/E1.dart
@@ -0,0 +1,34 @@
+void main(){
+
+  int age = 20;
+  double score = 10.5;
+  //int? speed;
+
+  //speed = 4;
+
+  late int speed;
+  speed = 3;
+ // print("speed:$speed ");
+  // age = 30;
+  // print(age);
+
+
+  //  null
+  // empty
+
+
+  String name = 'Flutter 12 #!';
+  name = "Myname";
+  //print(name);
+
+  bool check = true;
+       check = false;
+
+      // print(check);
+
+  var color = 12;
+  const car = "BMW";
+  final mobile = "Samsung";
+
+  //print(color.runtimeType);
+}
