37b8c881e1371d82272bff8dd3411e71dd3efeaa
hharddy <hharddy@yahoo.com>
2025-11-11 10:05:28 +0330

    S1E2 added

---
+++ S1/E2.dart
@@ -0,0 +1,43 @@
+void main(){
+
+  // var a = 21;
+  // var b = 5;
+  // print("a: $a");
+  //a  = 20 - 2;
+  //a = a - 4;
+  //a = a + 10;
+  // var c = a + b;
+  // print("a: $a");
+  // print("b: $b");
+ // print("c: ${ a-b }");
+  //var d = a/b;
+  //var d = a%b;
+  // var d = a*b;
+  // var f = "2";
+  // int casted = int.parse(f);
+  //
+  // print(casted.runtimeType);
+
+
+  /*
+  Unary operator
+   */
+
+  // var a = 10;
+ // ++a; a = a +1
+ //  a--; // a = a - 1
+ //  print("a: $a");
+
+
+  /*
+  Assignment operator
+   */
+  var a = 5.0;
+  var b = 3;
+  //a+=b; => a = a+b
+ // a-=2; => a = a- 2
+  // a/=5; a = a /5
+  // a+=1; a++, a = a +1
+  a =  a * 2;
+  print("a: $a");
+}
