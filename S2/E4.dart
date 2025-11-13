deffc181c9feca64a026e9f5dc5fc64be611308a
hharddy <hharddy@yahoo.com>
2025-11-13 09:36:03 +0330

    S2E4 code

---
+++ S2/E4.dart
@@ -0,0 +1,77 @@
+
+void main(){
+// >,<, >=,<=,==  , &&, ||,
+//   if ( 10 > 10 ) {
+//       print("10 is greater");
+//   }
+//   else if(10==10){
+//     print("Equal");
+//   }
+//
+//   else{
+//     print("10 is not greater...");
+//   }
+// var a=10;
+// var b = 2;
+
+// if(a>20 || b>10 ){
+//   print("a is greater than 20 ");
+//   print("b is greater than 10 ");
+// }else{
+//   print("false....");
+// }
+var a = "Hi";
+var b = "Hi";
+
+// if(a==b) print("a equal b");
+// else     print("not equal");
+
+// var c = false;
+// if(c){
+//   print("c is true");
+// }
+
+/*
+ switch case
+ */
+//
+// var c = 2;
+// switch(c){
+//   case 1:{
+//     print("c is 1");
+//   } break;
+//   case 2: {
+//     print("c is 2");
+//   }break;
+//   case 3: {
+//     print("c is 3");
+//   }break;
+//   default: {
+//     print("default...");
+//   }
+// }
+//
+// var f = "Hii";
+//
+// switch(f){
+//   case "Hi":{
+//     print("Hi");
+//   }break;
+//   case "Bye":{
+//     print("Bye");
+//
+//   }break;
+//   default:{
+//     print("Default...");
+//   }
+// }
+//
+
+/*
+  Ternary operator
+ */
+  var c=3;
+  //condition  ?  // true  :     // false
+var n = c>2? "c is greater":"c is not greater!";
+print(n);
+}
