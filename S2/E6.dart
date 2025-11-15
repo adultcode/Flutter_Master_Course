78eb9c0ff71f724250a22a4e9e19fe0fe017b8c0
hharddy <hharddy@yahoo.com>
2025-11-15 12:15:41 +0330

    S2E6

---
+++ S2/E6.dart
@@ -0,0 +1,109 @@
+void main(){
+
+  var a = 1;
+  var b = 0;
+  // while(a<5){
+  //   b = 0;
+  //   while(b<5){
+  //     print("$a,$b-Hello world");
+  //     b++;
+  //   }
+  //   a++;
+  // }
+
+  // while(a<100){
+  //   var c = a%3;
+  //   if(c==0){
+  //     print("$a");
+  //     b++;
+  //   }
+  //   a++;
+  // }
+  // print("Total: $b");
+
+  // do{
+  // print("Hello world");
+  // a++;
+  // }while(a<7);
+
+
+  /*
+  For c-style
+   */
+
+ // var f = 0;
+ //  var flag = 1;
+ //  for (var f = 1; f <5; f++  ){
+ //
+ //    for( var j = 0; j<5; j++){
+ //
+ //      print(" #$flag  F: $f - J: $j");
+ //      flag++;
+ //    }
+ //
+ //  }
+
+  // length = 5
+  // index: 0...4
+  var mylist = ["A","F","C","G","B"];
+
+  // for(var items = 0; items<mylist.length;items++){
+  //   print("item: "+mylist[items]);
+  // }
+
+  // for(var items in mylist){
+  //   print("item: $items");
+  // }
+
+  // mylist.forEach( (items) {
+  //   print("item: $items");
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+   Break
+   */
+// for(var c = 0; c<5; c++){
+//
+//   a = 0;
+//   while(a <10){
+//     print("a: $a");
+//     print("c: $c");
+//     if(a == 3){
+//       break;
+//     }
+//     a++;
+//   }
+//
+// }
+
+
+/*
+continue
+ */
+//
+// for(var f = 0; f<5; f++){
+//   if( f ==2 ){
+//     continue;
+//   }
+//   print("F: $f");
+// }
+
+ }
